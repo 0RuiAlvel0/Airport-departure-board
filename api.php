48db06aea4430fab64910f5563b22f7a529e122d
Rui Alves <info@commacmms.com>
2025-11-24 11:18:35 +0800

    Adjust for when arrival and dep results are not the same

--- api.php
+++ api.php
@@ -3,7 +3,7 @@
 	// error_reporting(E_ALL);
 	// ini_set('display_errors', 1);
 
-	require_once __DIR__ . '/vendor/autoload.php';
+	require_once __DIR__ . '/vendor/autoload.php'; 
 	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, null, true);
 	$dotenv->load();
 
